74c0f4b1791810ac7a6a2db88dc981a1f262e0d0
francisco-dvh <chicao_nfvsf@hotmail.com>
2021-09-11 00:36:54 -0300

    Routes

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -83,7 +83,7 @@ const DataTable = () => {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
-                    <tr>
+                      <tr>
                         <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
