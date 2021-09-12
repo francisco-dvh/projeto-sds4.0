8df6759aa7db130117a0e53f5255dee244d8ac2a
francisco-dvh <chicao_nfvsf@hotmail.com>
2021-09-12 12:52:53 -0300

    Correct Ending

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -47,7 +47,7 @@ const DataTable = () => {
                         {page.content?.map(item => (
 
                             <tr key={item.id}>
-                                <td>{formatLocalDate(item.date, "dd/MM/yyyyy")}</td>
+                                <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
                                 <td>{item.seller.name}</td>
                                 <td>{item.visited}</td>
                                 <td>{item.deals}</td>
